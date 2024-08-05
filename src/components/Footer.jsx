b90d8cec3ff1dbd662b58ee3e214594438b584fb
Rosa Jelves <rosa.sakura.j@gmail.com>
2024-08-04 20:53:36 -0400

    tercer intento arreglar centrado texto footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="container-fluid bg-dark">
-      <p className="text-center">© 2024 - Pizzería Mamma Mia! - Todos los derechos reservados</p>
+    <footer className="container-fluid bg-dark text-center">
+      <p>© 2024 - Pizzería Mamma Mia! - Todos los derechos reservados</p>
     </footer>
   );
 }
