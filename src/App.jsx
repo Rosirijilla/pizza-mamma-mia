c05a27900876ae64fc8bff3f5879ce638be91a4f
Rosa Jelves <rosa.sakura.j@gmail.com>
2024-08-24 23:23:50 -0400

    Hito de las Apis

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
-import Cart from "./components/Cart";
+//import Cart from "./components/Cart";
 //import Register from "./components/Register";
 //import Login from "./components/Login";
-import pizzas from "./components/pizzas.json"
+//import pizzas from "./components/pizzas.json"
 
-
-
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
 
-  return <>
-  <div>
-    <Navbar count={count}/>
-    <Cart pizzas={pizzas}/>
-    {/* <Home count={count} setCount={setCount}/>  */}
-    {/* <Register/> */}
-    {/* <Login/> */}
+  const obtenerInformacion = async () => {
+    const respuesta = await fetch("http://localhost:5000/api/pizzas");
+    const data = await respuesta.json();
+    setCount(data);
+    console.log(data)
+  };
+  useEffect(() => {
+    obtenerInformacion();
+  }, []);
 
-    <Footer/>
-  </div>
-  </>;
-}
+  return (
+    <>
+      <div>
+        <Navbar count={count} />
+        {/* <Cart pizzas={pizzas}/> */}
+        <Home count={count} setCount={setCount} />
+        {/* <Register/> */}
+        {/* <Login/> */}
+        <Footer />
+      </div>
+    </>
+  );
+};
 
 export default App;
